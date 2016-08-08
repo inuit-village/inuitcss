20f46c2c502c966071b236f821c666f9d7047098
Sean Tymon <tymon148@gmail.com>
2016-08-08 23:14:49 +0100

    [refs #133] adding widths test (doesn't work currently and tweaking test desc)

--- test/run.js
+++ test/run.js
@@ -6,7 +6,9 @@ var sassTrue = require('sass-true');
 // Test Files
 var fontSize = path.join(__dirname, '_tools.font-size.scss');
 var rem = path.join(__dirname, '_tools.rem.scss');
+var widths = path.join(__dirname, '_utilities.widths.scss');
 
 // Run Tests
 sassTrue.runSass({ file: fontSize }, describe, it);
 sassTrue.runSass({ file: rem }, describe, it);
+sassTrue.runSass({ file: widths }, describe, it);
